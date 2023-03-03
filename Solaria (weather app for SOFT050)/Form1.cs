ce93bbfcef600ef11bab306e73ef9a2f34bc81f8
mohdkhazaei <mohd.2132002@gmail.com>
2023-03-03 13:52:13 +0000

    Add project files.

---
+++ Solaria (weather app for SOFT050)/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Solaria__weather_app_for_SOFT050_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void curWeatherLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void curTempLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
